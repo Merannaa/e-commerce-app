d246593c0709f76fdb8522ee8fcc71fc1b5a1189
Merannaa <meran.ahmed.tr@gmail.com>
2024-08-02 21:58:47 +0300

    product apis

--- src/Middlewares/finders.middleware.js
+++ src/Middlewares/finders.middleware.js
@@ -18,3 +18,24 @@ export const getDocumentByName = (model) => {
     next();
   };
 };
+
+export const checkIdExist = (model)=>{
+  return async(req,res,next)=>{
+    const {category,subCategory,brand}=req.query
+    
+    const document = await model.findOne({
+      _id:brand,
+      categoryId:category,
+      subCategoryId:subCategory
+  }).populate([
+      {path:"categoryId",select:"customId"},
+      {path:"subCategoryId",select:"customId"}
+  ])
+
+  if(!document){
+      return next(new ErrorClass(`${model.modelName} is not found`,404))
+  }
+  req.document=document
+  next()
+  }
+}
